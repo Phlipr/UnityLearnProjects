201c45eddf2909101126f2fb8e0af74878d07904
Phillip Reynolds <preynolds1406@gmail.com>
2025-05-26 15:07:26 -0500

    Completed tutorial 2.2.

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     private readonly float xRange = 20.0f;
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,13 +16,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Keep the player in bounds
-        if (transform.position.x < -xRange) {
+        if (transform.position.x < -xRange)
+        {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange) {
+        if (transform.position.x > xRange)
+        {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
